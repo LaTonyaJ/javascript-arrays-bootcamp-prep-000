586c091b406a8b2c4474d4df884a49b14b4c5a37
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-24 20:42:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  [element,...array]
+  element = array.unshift()
   return array 
 }
