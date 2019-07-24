7944a3e081a442d10813d704b1a3e1dff44235fe
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-24 21:12:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array){
+  array.slice(0)
+  return array
+}
