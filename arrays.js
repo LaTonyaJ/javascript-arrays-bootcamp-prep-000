83b29ea05661563c90ecfc9c233fb57d0ea0f087
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-24 18:09:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var superheroines = ['cat woman', 'she-hulk', 'mystique'];
+superheroines.push('wonder woman');
+
+var cities = ['New York', 'San Francisco'];
+cities.unshift('Philadelphia');
+
+var cities = ['New York', 'San Francisco'];
+['Philadelphia',...cities];
+
+function addElementToBeginningOfArray(array, element){
+  array = []
+  array.unshift()
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
