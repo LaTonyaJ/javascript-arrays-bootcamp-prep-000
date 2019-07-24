474beca340e4fc1c83b80ed5d5dc0b57ca2669eb
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-24 21:36:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -47,7 +47,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
+  array = array.slice(0, array.length - 1)
   return array
 }
 
