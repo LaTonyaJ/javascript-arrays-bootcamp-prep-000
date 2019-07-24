dab1101016fbc21eabcbe3169c14b16bfd750b70
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-24 18:11:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,8 +10,8 @@ var cities = ['New York', 'San Francisco'];
 ['Philadelphia',...cities];
 
 function addElementToBeginningOfArray(array, element){
-  array = []
-  array.unshift()
+  array = [1]
+  element = array.unshift()
   return array
 }
 
